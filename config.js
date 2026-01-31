1f8d4418b4d74a1bb8eab17da2c03929786d769c
GeorgianaAndreea98 <georgianaanton56@yahoo.com>
2026-01-31 20:27:18 +0100

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dellfqzzn/video/upload/v1769886646/LeleMusic_-_Mai_cer_de_la_cer_zile_sa_le_petrec_cu_tine___Manele_VTM_4tfISQtNjoc_sdgjgg.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
